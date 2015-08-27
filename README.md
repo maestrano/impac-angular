36cd19ec7b29aba2d9086b24df253b23a41bf03f
Xaun Lopez <xaunlopez@gmail.com>
2015-08-27 16:54:24 +1000

    Gulp task for templateCaching impac html templates, and concatinating angular-template.module.js & templateCache code into a dist file.

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
+# impac-angular
+---
 
+#### Information
+
+- Templates for directives must not use double-quotes for strings. Only html attribute values may be wrapped in double qoutes.
+
+#### Version 2
+
+- CoffeeScript sourcemap files
 
--- gulpfile.js
+++ gulpfile.js
@@ -8,15 +8,81 @@ var gulp = require('gulp'),
     ngAnnotate = require('gulp-ng-annotate'),
     pkg = require('./package.json'),
     sourceFiles = [
-      'src/impacAngular/impacAngular.prefix',
-      'src/impacAngular/impacAngular.js',
-      'src/impacAngular/directives/**/*.js',
-      'src/impacAngular/filters/**/*.js',
-      'src/impacAngular/services/**/*.js',
-      'src/impacAngular/impacAngular.suffix'
+      'src/impac-angular/impac-angular.prefix',
+      'src/impac-angular/impac-angular.js',
+      'src/impac-angular/directives/**/*.js.coffee',
+      'src/impac-angular/filters/**/*.js.coffee',
+      'src/impac-angular/services/**/*.js.coffee',
+      'src/impac-angular/impac-angular.suffix'
     ];
 
-gulp.task('build', function() {
+/*
+** Testing Tasks
+*/
+// // Run test once and exit
+// gulp.task('test', function (done) {
+//   karma.start({
+//     configFile: __dirname + '/karma-src.conf.js',
+//     singleRun: true
+//   }, done);
+// });
+
+// gulp.task('test-debug', function (done) {
+//   karma.start({
+//     configFile: __dirname + '/karma-src.conf.js',
+//     singleRun: false,
+//     autoWatch: true
+//   }, done);
+// });
+
+// // Run test once and exit
+// gulp.task('test-dist-concatenated', function (done) {
+//   karma.start({
+//     configFile: __dirname + '/karma-dist-concatenated.conf.js',
+//     singleRun: true
+//   }, done);
+// });
+
+// // Run test once and exit
+// gulp.task('test-dist-minified', function (done) {
+//   karma.start({
+//     configFile: __dirname + '/karma-dist-minified.conf.js',
+//     singleRun: true
+//   }, done);
+// });
+
+/* **************************
+** Template Caching Tasks ***
+** *************************/
+// Builds $templateCache code from each of the html files within the src directory,
+// outputting it into a file named templates.tmp at the root destination.
+gulp.task('templates', function () {
+  return gulp.src('src/impac-angular/templates/**/*.html')
+    .pipe(templates('templates.tmp', {
+      // root: '/templates/',
+      // module: pkg.name
+      module: 'maestrano.analytics.templates'
+    }))
+    .pipe(gulp.dest('.'));
+});
+
+// makes a copy of impac-angular.modules.js and concatinates templates.tmp into it.
+gulp.task('concat', ['templates'], function () {
+  return gulp.src(['src/impac-angular/impac-angular.module.js', 'templates.tmp'])
+    .pipe(concat(pkg.name + '.js')) // output filename
+    .pipe(gulp.dest('./src/impac-angular/')); // output destination
+});
+
+// cleans up tmp file created by 'templates' task.
+gulp.task('clean', ['concat'], function () {
+  gulp.src('./*.tmp', {read: false})
+    .pipe(clean());
+});
+
+/* **********************
+** Build tasks        ***
+** **********************/
+gulp.task('build', ['clean'], function() {
   gulp.src(sourceFiles)
     .pipe(concat('impac-angular.js'))
     .pipe(ngAnnotate())
@@ -26,78 +92,24 @@ gulp.task('build', function() {
     .pipe(gulp.dest('./dist'));
 });
 
-/**
- * Run test once and exit
- */
-gulp.task('test', function (done) {
-  karma.start({
-    configFile: __dirname + '/karma-src.conf.js',
-    singleRun: true
-  }, done);
+gulp.task('watch', function () {
+  gulp.watch(['src/**/*.js', 'src/**/*.html'], ['build']);
 });
 
-gulp.task('test-debug', function (done) {
-  karma.start({
-    configFile: __dirname + '/karma-src.conf.js',
-    singleRun: false,
-    autoWatch: true
-  }, done);
-});
-
-/**
- * Run test once and exit
- */
-gulp.task('test-dist-concatenated', function (done) {
-  karma.start({
-    configFile: __dirname + '/karma-dist-concatenated.conf.js',
-    singleRun: true
-  }, done);
-});
-
-/**
- * Run test once and exit
- */
-gulp.task('test-dist-minified', function (done) {
-  karma.start({
-    configFile: __dirname + '/karma-dist-minified.conf.js',
-    singleRun: true
-  }, done);
-});
-
-gulp.task('templates', function () {
-  return gulp.src('src/impacAngular/templates/**/*.html')
-    .pipe(templates('templates.tmp', {
-      // root: '/templates/',
-      // module: pkg.name
-      module: 'maestrano.analytics.templates'
-    }))
-    .pipe(gulp.dest('.'));
-});
-
-gulp.task('concat', ['templates'], function () {
-  return gulp.src([pkg.main, 'templates.tmp'])
-    .pipe(concat(pkg.name + '.js'))
-    .pipe(gulp.dest('./dist/'));
-});
-
-gulp.task('clean', ['concat'], function () {
-  gulp.src('./*.tmp', {read: false})
-    .pipe(clean());
-});
-
-gulp.task('default', ['test', 'build']);
-
-gulp.task('dist',
-  [
-    'test',
-    'test-dist-concatenated',
-    'test-dist-minified',
-    'templates',
-    'concat',
-    'clean'
-  ]);
-
-gulp.task('templates', ['templates', 'concat', 'clean']);
+/* **********************
+** Commands           ***
+** **********************/
+gulp.task('default', ['templates', 'concat', 'clean', 'build']);
+// gulp.task('foobar', ['build']);
+// gulp.task('dist',
+//   [
+//     'test',
+//     'test-dist-concatenated',
+//     'test-dist-minified',
+//     'templates',
+//     'concat',
+//     'clean'
+//   ]);
 
 
 
