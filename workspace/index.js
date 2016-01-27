e6014153c9800c42513b53ac3a4b0a4bc0129422
Xaun Lopez <xaunlopez@gmail.com>
2016-01-27 16:37:37 +1100

    added kpis, and impac paths to workspace config + cleanups & comments

--- workspace/index.js
+++ workspace/index.js
@@ -1,24 +1,20 @@
 var module = angular.module('impacWorkspace', ['maestrano.impac']);
 
 // Configuration impac-angular lib on module impacWorkSpace run.
-module.config(function ($httpProvider) {
-  // $httpProvider.defaults.headers.common['Accept'] = 'application/json';
-  // $httpProvider.defaults.headers.common['Content-Type'] = 'application/json';
-  // $httpProvider.defaults.useXDomain = true;
-  // delete $httpProvider.defaults.headers.common['X-Requested-With'];
-});
 module.run(function ($log, $window, $q, $http, ImpacLinking, ImpacRoutes) {
 
   // TODO: set-up server to enable local $http calls to setting.json
   var settings = {
-    url: 'http://localhost:3000',
+    mno_url: 'http://localhost:3000',
+    impac_url: 'http://localhost:4000',
     api_key: '7149f0c91d7e995c10be79ed4881c035d662632995e852f550313345e0f0b982',
     api_secret: '4a776ea4-8134-4f38-b632-85cbe951524e'
   };
 
   // encodes a base64 string
   var credentials = $window.btoa(settings.api_key + ':' + settings.api_secret);
-
+  // attaches basic auth onto $http default, which configures all impacWorkspace & maestrano.impac
+  // angular modules $http requests.
   $http.defaults.headers.common.Authorization = 'Basic ' + credentials;
 
   ImpacLinking.linkData({
@@ -26,22 +22,27 @@ module.run(function ($log, $window, $q, $http, ImpacLinking, ImpacRoutes) {
       return getOrganizations();
     },
     user: function () {
-      return $q.when({});
+      return $q.when({ name: 'Developer' });
     }
   });
 
   ImpacRoutes.configureRoutes({
-    dhbBasePath: settings.url + '/api/v2/impac/dashboards',
-    widgetBasePath: settings.url + '/api/v2/impac/widgets',
-    // impacKpisBasePath: ''
+    // mno paths
+    dhbBasePath: settings.mno_url + '/api/v2/impac/dashboards',
+    widgetBasePath: settings.mno_url + '/api/v2/impac/widgets',
+    kpiBasePath: settings.mno_url + '/api/v2/impac/kpis',
+    // impac paths
+    showWidgetPath: settings.impac_url + '/api/v1/get_widget',
+    impacKpisBasePath: settings.impac_url + '/api/v2/kpis'
   });
 
   function getOrganizations() {
-    return $http.get(settings.url + '/api/v2/impac/organizations')
+    return $http.get(settings.mno_url + '/api/v2/impac/organizations')
       .then(function (response) {
         var organizations = (response.data || []);
         return { organizations: organizations, currentOrgId: (organizations[0].id || null) };
-      }
+      },
+      fail
     );
   }
 
