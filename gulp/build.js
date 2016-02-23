506bb7bcff042847eb070f5a231254ecef57800c
Xaun Lopez <xaunlopez@gmail.com>
2016-02-23 16:49:30 +1100

    bugfixes and cleanups across gulp and bower

--- gulp/build.js
+++ gulp/build.js
@@ -13,29 +13,28 @@ gulp.task('partials', function () {
   return gulp.src([
     path.join(conf.paths.src, '/components/**/*.html')
   ])
-    .pipe($.minifyHtml({
-      empty: true,
-      spare: true,
-      quotes: true,
-      loose: true
-    }))
+    // TODO: No combination of below's options work; all break the templates.
+    //       Look into how to successfully minify impac-angulars templates.
+    // .pipe($.minifyHtml({
+    //   empty: true,
+    //   spare: true,
+    //   quotes: true,
+    //   loose: true
+    // }))
     .pipe($.angularTemplatecache('templateCacheHtml.js', {
       module: 'impac.components.templates', // module name
-      standalone: 'true', // creates a new module
-      // TODO-XAUN: needed?
+      standalone: true, // creates a new module
       // Shorten component $templaceCache paths for simpler dynamic selection, and
       // cleaner includes.
       transformUrl: function (url) {
-            // parent component e.g dashboard, widgets.
-        var parentFolderName  = url.split('/').splice(0, 1),
-            // component's template name.
-            fileName          = url.split('/').splice(-1, 1);
-
+        // e.g widgets/accounts-balance/accounts-balance.tmpl.html
+        var parentFolderName  = url.split('/').slice(0, 1);
+        var fileName          = url.split('/').slice(-1);
         // if html file is a modal, return full path for semantic purposes.
         if (fileName[0].indexOf('.modal.') >= 0) {
           return url;
         }
-        // e.g widgets/accounts-balance
+        // e.g widgets/accounts-balance.tmpl.html
         return parentFolderName + '/' + fileName;
       }
     }))
@@ -48,7 +47,7 @@ gulp.task('clean', function (asyncCallback) {
 });
 
 gulp.task('build', ['scripts', 'styles', 'partials'], function() {
-  // Source files for final dist build
+  // Source files for final dist build - NOTE: order is imporant.
   var buildSourceFiles = [
     path.join(conf.paths.src, 'impac-angular.prefix'),
     path.join(conf.paths.src, 'impac-angular.module.js'),
